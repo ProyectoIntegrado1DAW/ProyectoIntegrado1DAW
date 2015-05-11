3f332eab83e92d729837a0823e6a5aad130ee2a0
Hector <hector151293@gmail.com>
2015-05-11 10:07:17 +0200

    BBDD actualizada
    Signed-off-by: Hector <hector151293@gmail.com>

--- Base de datos/clicktick.sql
+++ Base de datos/clicktick.sql
@@ -15,6 +15,9 @@ CREATE TABLE clientes (
 	Nombre varchar(30) NOT NULL,
 	Apellido varchar(30) NOT NULL,
 	Direccion varchar(50) NOT NULL,
+	Poblacion varchar(50) NOT NULL,
+	Provincia varchar(50) NOT NULL,
+	CodPostal varchar(50) NOT NULL,
 	Pais varchar(20) NOT NULL,
 	Telefono varchar(20) NOT NULL,
 	Email varchar(30) NOT NULL,
@@ -23,11 +26,16 @@ CREATE TABLE clientes (
 
 CREATE TABLE gestores (
 	DNI varchar(10) NOT NULL,
+	Usuario varchar(15) NOT NULL,
+	Contrasenya varchar(10) NOT NULL,
 	Nombre varchar(30) NOT NULL,
 	Apellido varchar(30) NOT NULL,
 	Direccion varchar(50) NOT NULL,
+	Poblacion varchar(50) NOT NULL,
+	Provincia varchar(50) NOT NULL,
+	CodPostal varchar(50) NOT NULL,
 	Pais varchar(20) NOT NULL,
-	Telefono integer NOT NULL,
+	Telefono varchar(20) NOT NULL,
 	Email varchar(30) NOT NULL,
 	PRIMARY KEY (DNI)
 );
@@ -39,12 +47,13 @@ CREATE TABLE eventos (
 	TipoEvento varchar(20) NOT NULL,
 	Locales varchar(20) NOT NULL,
 	Ciudad varchar(20) NOT NULL,
-	TipoEntrada varchar(10) NOT NULL,
+	NumEntradas integer NOT NULL,
+	EntradasReservadas integer default NULL,
+	Descripcion varchar(100) default NULL,
 	Precio decimal NOT NULL,
 	Fecha date NOT NULL,
 	DiaSemana varchar(15) NOT NULL,
 	Hora varchar(20) NOT NULL,
-	Estado varchar(15) NOT NULL,
 	PRIMARY KEY (IDEvento),
 	CONSTRAINT eventos_DNI_FK FOREIGN KEY (DNI) REFERENCES gestores (DNI)
 );
