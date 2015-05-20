658e3e28dcc6b203e649ad88b1a2ef84f8136d5b
Miquel <mmg9995@gmail.com>
2015-05-20 10:04:58 +0200

    Signed-off-by: Miquel <mmg9995@gmail.com>

--- ProyectoDAW/src/gestores/GestorEntrada.java
+++ ProyectoDAW/src/gestores/GestorEntrada.java
@@ -1,42 +1,88 @@
 package gestores;
+
+import java.io.File;
+import java.sql.SQLException;
+
+import javax.xml.transform.TransformerException;
+
+import entidades.Entrada;
 import entidades.Evento;
 import entidades.Cliente;
 
 public class GestorEntrada {
-	
-	public static void reservarEntrada(){}
-	
-	public static void compraEntrReg(Evento evento){
-		
-		Cliente client = Cliente.getInstance();
-		
-		}
-	
-	
-	public static void compraEntr(Evento evento, Cliente cliente){
-		
+
+	public static void reservarEntrada() {
+	}
+
+	// public static void compraEntrReg(Evento evento) {}
+
+	public static void compraEntr(Evento evento, Entrada entrada, int numEnt) {
+
 		// llamara a existePlazasDisponibles
-		
+
 		// si quedan entradas, hara la compra y actualizara la base de datos.
 		// Restando las entradas compradas a las dispobnibles
-		
+
 		// finalmente imprimira la entrada
+
+	}
 	
+	public static void generarEntrada(Evento evento, Cliente cliente, int numEnt) {
+		
+		Evento ev = GestorEvento.obtenerEvento(evento.getNombre());
+		int ultimoID = ev.getEntrReservadas();
+		
+		for (int i = 0; i < numEnt; i++) {
+			// id buscar el num max reservada i a partir de ahi buscar
+			
+			Entrada e = new Entrada(IDentrada,IDevento,DNI,
+					nombreEvento, informacion,tipoEntrada,
+					Precio, datosCliente);
+			imprimirEntrada(e);
 		}
-	
-	public static void imprimirEntrada(Evento evento, Cliente cliente){}
-	
-	public static boolean existePlazasDisponibles(Evento evento){
-		//conectar base de datos para ver si existen plazas
+		
+		
+	}
+
+	public static void imprimirEntrada(Entrada entrada) {
+		
+		ConversorXML marshaller = new ConversorXML(entrada);
+
+		marshaller.crearDocumento();
+		marshaller.crearArbolDOM();
+
+		File file = new File("ticket2.xml");
+
+		try {
+			marshaller.escribirDocumentoAXml(file);
+		} catch (TransformerException ex) {
+
+		}
+		
+	}
+
+	public static boolean existePlazasDisponibles(Evento evento, int numEnt)
+			throws SQLException {
+		// conectar base de datos para ver si existen plazas
 		boolean existen = false;
-		
-		
+
+		Evento ev = GestorEvento.obtenerEvento(evento.getNombre());
+		if ((ev.getNumEntradas() - ev.getEntrReservadas()) > numEnt) {
+			existen = true;
+		}
+
 		return existen;
 	}
-	
-	public static void actualEntradas(Evento evento){
-		// actualizar los datos de la base de datos, restar numero de entradas a disponibles.
+
+	public static void actualEntradas(Evento evento, int numEnt) throws SQLException {
+
+		Evento ev = GestorEvento.obtenerEvento(evento.getNombre());
+		ConexionDB conexion = ConexionDB.getConexionDB();
+		int i = ev.getEntrReservadas() + numEnt;
+
+		conexion.setQuery("UPDATE clickntick.eventos SET EntradasReservadas = " + i
+				+ " WHERE NombreEvento = '" + ev.getNombre() + "';");
+
 	}
-	
 
 }
--- ProyectoDAW/src/gestores/GestorEvento.java
+++ ProyectoDAW/src/gestores/GestorEvento.java
@@ -4,6 +4,7 @@ package gestores;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import entidades.Evento;
 
@@ -91,6 +92,56 @@ public class GestorEvento {
 
 	}
 
+	public static ArrayList getArrayEvento() throws SQLException{
+		ArrayList<Evento> eventos  = new ArrayList<Evento>();
+		
+		Evento e = null;
+		ConexionDB conexion = ConexionDB.getConexionDB();
+		ResultSet resultado;
+
+		String nombre = null;
+		String tipoEvento = null;
+
+		int numEntradas = 0;
+		int entrReservadas = 0;
+
+		String descripcion = null;
+		String locales = null;
+		String ciudad = null;
+		Date fecha = null;
+		int precio = 0;
+
+		String DiaSemana = null;
+		String hora = null;
+
+		resultado = conexion
+				.getQuery("SELECT * FROM clickntick.eventos;");
+
+		while (resultado.next()) {
+
+			nombre = resultado.getString("nombreevento");
+			tipoEvento = resultado.getString("tipoevento");
+			numEntradas = resultado.getInt("numentradas");
+			entrReservadas = resultado.getInt("entradasreservadas");
+			descripcion = resultado.getString("descripcion");
+			locales = resultado.getString("locales");
+			ciudad = resultado.getString("ciudad");
+			fecha = resultado.getDate("fecha");
+			precio = resultado.getInt("precio");
+			DiaSemana = resultado.getString("diasemana");
+			hora = resultado.getString("hora");
+			
+			e = new Evento(nombre, tipoEvento, locales, ciudad,
+					numEntradas, entrReservadas, descripcion, precio, fecha.toString(),
+					DiaSemana, hora);
+			
+			eventos.add(e);
+
+		}
+		
+		return eventos;
+	}
+	
 	public static void bajaEvento(Evento evento) {
 
 		ConexionDB conexion = ConexionDB.getConexionDB();
